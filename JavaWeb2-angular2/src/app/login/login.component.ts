023e8d0579801ac721179b44d4d536ebbcd9fe63
tank2140896 <631094182@qq.com>
2017-02-19 18:54:49 +0800

    no message

---
+++ JavaWeb2-angular2/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import 'core-js';
+import 'zone.js';
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Component} from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+    declarations: [LoginComponent],
+    imports: [BrowserModule],
+    bootstrap: [LoginComponent]
+})
+
+@Component({
+    selector: 'web-app',
+    templateUrl: 'app/login/login.html',
+    styleUrls: ['app/login/login.css']
+})
+export class LoginComponent{}
+
+platformBrowserDynamic().bootstrapModule(LoginComponent);//.catch(err => console.error(err));
