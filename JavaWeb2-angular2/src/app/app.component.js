88d93dcbff47b291ded77cce9018f13e1312691a
tank2140896 <631094182@qq.com>
2017-03-11 21:41:31 +0800

    no message

--- JavaWeb2-angular2/src/app/app.component.js
+++ JavaWeb2-angular2/src/app/app.component.js
@@ -5,7 +5,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 //import {Observable} from 'rxjs/Rx';
 var login_user_1 = require("./models/login/login.user");
 var AppComponent = (function () {
@@ -50,7 +54,8 @@ AppComponent = __decorate([
         templateUrl: 'app/login/login.html',
         styleUrls: ['app/login/login.css']
     }),
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
