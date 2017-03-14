39ffc6bf6d88efa97f21d70e7f7ed26dda6788fa
tank2140896 <631094182@qq.com>
2017-03-14 20:58:01 +0800

    no message

--- JavaWeb2-angular2/src/app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require("core-js");
-require("zone.js");
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-//import {enableProdMode} from '@angular/core';
-var app_module_1 = require("./app.module");
-//enableProdMode();//生产环境
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule).catch(function (err) { return console.error(err); });
-//# sourceMappingURL=main.js.map
