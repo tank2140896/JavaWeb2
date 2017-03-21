2f8ae9b3c242fc712fcde09873bdfea3a1969908
MrRiver <411833172@qq.com>
2017-03-21 17:51:57 +0800

    Update SystemConstant.java

--- JavaWeb2-core/src/main/java/com/javaweb/constant/SystemConstant.java
+++ JavaWeb2-core/src/main/java/com/javaweb/constant/SystemConstant.java
@@ -2,8 +2,8 @@ package com.javaweb.constant;
 
 public final class SystemConstant {
 	
-	public static final String SYSTEM_DEFAULT_CREATOR = "system";
+	public static final String SYSTEM_DEFAULT_CREATOR = "超级管理员";
 	
-	public static final String SYSTEM_DEFAULT_UPDATER = "system";
+	public static final String SYSTEM_DEFAULT_UPDATER = "超级管理员";
 
 }
