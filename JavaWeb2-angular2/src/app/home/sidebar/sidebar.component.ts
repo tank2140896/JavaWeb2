50f21801bd292766a0a03a4d327d80114061368c
tank2140896 <631094182@qq.com>
2017-04-02 00:04:08 +0800

    no message

--- JavaWeb2-angular2/src/app/home/sidebar/sidebar.component.ts
+++ JavaWeb2-angular2/src/app/home/sidebar/sidebar.component.ts
@@ -12,18 +12,16 @@ export class SidebarComponent {
         this.getMenuList(JSON.parse(window.sessionStorage.getItem('menuList')));
     }
 
-    htmlWrite:string = '';
+    htmlContent:string = '';
 
     getMenuList(menuList:any):void{
         for(let i=0;i<menuList.length;i++){
             let each = menuList[i];
             if(each.list!=null&&each.list.length>0){
-                this.htmlWrite += '<div>'+each.modulename+'</div>';
+                this.htmlContent += '<div>'+each.modulename+'</div>';
                 this.getMenuList(each.list);
             }else{
-                //http://stackoverflow.com/questions/36325212/angular-2-dynamic-tabs-with-user-click-chosen-components/36325468
-                //NgComponentOutlet
-                this.htmlWrite += '<div><a href="#" [routerLink]="'+each.moduleurl+'">'+each.modulename+'</a></div>';
+                this.htmlContent += '<div><a href="#/home/'+each.moduleurl+'">'+each.modulename+'</a></div>';
             }
         }
     }
