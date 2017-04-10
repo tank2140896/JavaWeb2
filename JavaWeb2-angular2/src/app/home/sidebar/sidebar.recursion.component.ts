1dd1c838cb8d1fc8617d1cfc6f096b7c9f24ce21
tank2140896 <631094182@qq.com>
2017-04-10 23:26:10 +0800

    no message

--- JavaWeb2-angular2/src/app/home/sidebar/sidebar.recursion.component.ts
+++ JavaWeb2-angular2/src/app/home/sidebar/sidebar.recursion.component.ts
@@ -2,13 +2,7 @@ import {Component,Input} from "@angular/core";
 
 @Component({
     selector: 'sidebar-recursion',
-    template: `
-                <div *ngIf="list.length==0"><a [routerLink]="[url]">{{name}}</a></div>
-                <div *ngIf="list.length>0">{{name}}</div>
-                <div *ngIf="list.length>0">
-                    <sidebar-recursion *ngFor="let i of list" [url]="i.pageUrl" [name]="i.moduleName" [list]="i.list"></sidebar-recursion>
-                </div>            
-        `,
+    templateUrl: 'sidebar.recursion.html',
     styleUrls: ['sidebar.recursion.css']
 })
 
