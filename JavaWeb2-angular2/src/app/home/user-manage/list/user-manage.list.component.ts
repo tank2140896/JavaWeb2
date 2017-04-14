3af2ce0a63cde50498c9e1e265779076ac389325
tank2140896 <631094182@qq.com>
2017-04-14 22:07:26 +0800

    no message

--- JavaWeb2-angular2/src/app/home/user-manage/list/user-manage.list.component.ts
+++ JavaWeb2-angular2/src/app/home/user-manage/list/user-manage.list.component.ts
@@ -3,6 +3,7 @@ import {HttpService} from "../../../service/http/HttpService";
 import {HttpRequestUrl} from "../../../constant/HttpRequestUrl";
 import {UserSearch} from "../../../models/user/user.search";
 import {AuthService} from "../../../service/auth/AuthService";
+import {HeadToken} from "../../../models/token/head.token";
 
 @Component({
     selector: 'user-manage-list',
@@ -23,9 +24,12 @@ export class UserManageListComponent implements OnInit {
     private totalPage:number;
 
     ngOnInit(): void {
+        let headToken = new HeadToken();
+        headToken.userId = JSON.parse(window.sessionStorage.getItem('loginSuccessData')).user.userId;
+        headToken.token = JSON.parse(window.sessionStorage.getItem('loginSuccessData')).token;
         this.httpService.postJsonData(HttpRequestUrl.SYS_USER_LSIT,
                                       JSON.stringify(this.userSearch),
-                                      JSON.parse(window.sessionStorage.getItem('loginSuccessData')).token).subscribe(
+                                      headToken).subscribe(
             result=>{
                 this.listData = result.data.data;
                 this.currentPage = result.data.currentPage;
