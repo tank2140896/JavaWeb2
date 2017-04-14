69c353222192686d42b17dde243a476130e9c4a0
MrRiver <411833172@qq.com>
2017-04-14 16:57:09 +0800

    Create TokenCheck.java

---
+++ JavaWeb2-api/src/main/java/com/javaweb/dataobject/eo/TokenCheck.java
@@ -0,0 +1,25 @@
+package com.javaweb.dataobject.eo;
+
+public class TokenCheck {
+	
+	private String token;
+	
+	private Long currentTime;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Long getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(Long currentTime) {
+		this.currentTime = currentTime;
+	}
+	
+}
