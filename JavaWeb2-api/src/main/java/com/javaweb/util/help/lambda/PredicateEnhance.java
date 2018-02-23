4f5101a371f878354b83b19f9b39456bc3a78b57
tank2140896 <411833172@qq.com>
2018-02-23 14:11:04 +0800

    no message

---
+++ JavaWeb2-api/src/main/java/com/javaweb/util/help/lambda/PredicateEnhance.java
@@ -0,0 +1,7 @@
+package com.javaweb.util.help.lambda;
+
+import java.util.function.Predicate;
+
+public interface PredicateEnhance<T> extends Predicate<T> {
+
+}
---
+++ JavaWeb2-api/src/main/java/com/javaweb/util/help/lambda/SupplierEnhance.java
@@ -0,0 +1,7 @@
+package com.javaweb.util.help.lambda;
+
+import java.util.function.Supplier;
+
+public interface SupplierEnhance<T> extends Supplier<T> {
+
+}
