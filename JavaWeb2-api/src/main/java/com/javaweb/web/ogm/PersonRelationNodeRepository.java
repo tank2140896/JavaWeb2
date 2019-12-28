22ff24d1f114b4234ce3a964848db3ecdef7ce13
he-PC\he <411833172@qq.com>
2019-12-28 13:32:51 +0800

    no message

---
+++ JavaWeb2-api/src/main/java/com/javaweb/web/ogm/PersonRelationNodeRepository.java
@@ -0,0 +1,9 @@
+package com.javaweb.web.ogm;
+
+/**
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+public interface PersonRelationNodeRepository extends Neo4jRepository<PersonRelationNodeEntity,String> { 
+
+}
+*/
