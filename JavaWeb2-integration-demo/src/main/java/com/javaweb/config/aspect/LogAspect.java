42b12518cddb66820987cd76d41ac5cc80767d9d
MrRiver <411833172@qq.com>
2020-03-30 11:13:02 +0800

    Update LogAspect.java

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/aspect/LogAspect.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/aspect/LogAspect.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.javaweb.util.core.HttpUtil;
+
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONObject;
 
@@ -27,22 +29,33 @@ public class LogAspect {
 		ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
 		HttpServletRequest httpServletRequest = servletRequestAttributes.getRequest();
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("请求地址:").append(httpServletRequest.getRequestURL().toString()).append(",")
-		             .append("请求方式:").append(httpServletRequest.getMethod()).append(",")
-		             .append("请求类方法:").append(joinPoint.getSignature()).append(",")
-		             .append("请求类方法参数:").append(Arrays.toString(joinPoint.getArgs()));
-	    /**
+		stringBuilder.append("请求URL:").append(httpServletRequest.getRequestURL().toString())
+					 .append(",")
+		             .append("请求方式:").append(httpServletRequest.getMethod())
+		             .append(",")
+		             .append("请求类方法:").append(joinPoint.getSignature())
+		             .append(",")
+		             .append("请求类方法参数:").append(Arrays.toString(joinPoint.getArgs()))
+					 .append(",")
+					 .append("请求IP:").append(HttpUtil.getIpAddress(httpServletRequest));
+		/**
+		String url = httpServletRequest.getRequestURL().toString();
+		if(url.contains("add")||url.contains("modify")||url.contains("delete")){
+			
+		}
+		*/
+	        /**
 		Object obj[] = joinPoint.getArgs();
-	    for(int i=0;i<obj.length;i++) {
-	        Object o = obj[i];
-	        try {
-	            obj[i] = JSONObject.fromObject(o).toString();
-	        }catch (Exception e) {
-                //do nothing
-            }
-	    }
-	    */
-	    log.info(stringBuilder.toString());
+	        for(int i=0;i<obj.length;i++) {
+	            Object o = obj[i];
+	            try {
+	                obj[i] = JSONObject.fromObject(o).toString();
+	            }catch (Exception e) {
+                    //do nothing
+                    }
+	        }
+	        */
+	        log.info(stringBuilder.toString());
 	}
 	
 	@AfterReturning(value=DEFAULT_LOG_POINT_CUT,returning="object")
