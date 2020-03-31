16a9510fdd5d0f1af797657eae442d0dad80c05b
MrRiver <411833172@qq.com>
2020-03-31 09:12:49 +0800

    Update BaseEntity.java

--- JavaWeb2-common/src/main/java/com/javaweb/base/BaseEntity.java
+++ JavaWeb2-common/src/main/java/com/javaweb/base/BaseEntity.java
@@ -9,12 +9,26 @@ import lombok.Setter;
 @Setter
 public class BaseEntity extends BaseValidatedGroup {
 	
+	/**
+	import java.util.Date;
+	import com.fasterxml.jackson.annotation.JsonFormat;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	@Column(name="create_date")
+	private Date createDate;
+	*/
 	@Column(name="create_date")
 	private String createDate;//创建时间
 	
 	@Column(name="creator")
 	private String creator;//创建者
 	
+	/**
+	import java.util.Date;
+	import com.fasterxml.jackson.annotation.JsonFormat;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	@Column(name="update_date")
+	private Date updateDate;
+	*/
 	@Column(name="update_date")
 	private String updateDate;//更新时间
 	
