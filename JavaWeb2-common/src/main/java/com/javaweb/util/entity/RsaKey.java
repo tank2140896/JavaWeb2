825c5f240c51c62572d1f11b42447e04f7a10426
MrRiver <411833172@qq.com>
2020-03-31 13:24:44 +0800

    Create RsaKey.java

---
+++ JavaWeb2-common/src/main/java/com/javaweb/util/entity/RsaKey.java
@@ -0,0 +1,14 @@
+package com.javaweb.util.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RsaKey {
+	
+	private String rsaPublicKey;
+	
+	private String rsaPrivateKey;
+	
+}
