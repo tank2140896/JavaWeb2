3a59f4b8f6b7ce4bde587d221ef7099a06e6f990
MrRiver <411833172@qq.com>
2020-04-15 10:56:27 +0800

    Update InterfacesService.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/InterfacesService.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/InterfacesService.java
@@ -2,11 +2,16 @@ package com.javaweb.web.service;
 
 import com.javaweb.util.entity.Page;
 import com.javaweb.web.eo.interfaces.InterfacesListRequest;
+import com.javaweb.web.po.Interfaces;
 
 public interface InterfacesService {
 	
 	public void synchronizedInterfaces();
 	
 	public Page interfacesList(InterfacesListRequest interfacesListRequest);
+	
+	public Interfaces interfacesDetail(String interfacesId);
+	
+	public void interfacesModify(Interfaces interfaces);
 
 }
