0d90e39857260af7457a2a7dcb88f6b4175799ca
tank2140896 <411833172@qq.com>
2020-04-16 20:05:42 +0800

    no message

--- JavaWeb2-eureka-client-zuul/src/main/java/com/javaweb/filter/MyZuulFilter.java
+++ JavaWeb2-eureka-client-zuul/src/main/java/com/javaweb/filter/MyZuulFilter.java
@@ -1,17 +1,18 @@
 package com.javaweb.filter;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.javaweb.base.BaseResponseResult;
 import com.javaweb.constant.CommonConstant;
 import com.javaweb.context.ApplicationContextHelper;
-import com.javaweb.enums.HttpCodeEnum;
-import com.javaweb.web.eo.TokenData;
+import com.javaweb.util.core.HttpUtil;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -20,6 +21,8 @@ import com.netflix.zuul.exception.ZuulException;
 public class MyZuulFilter extends ZuulFilter {
     
     private RedisTemplate<String,Object> redisTemplate = null;
+    public static final int COUNT_TIMES = 3;//3次次数限制
+    public static final int LIMIT_TIME = 5;//5秒时间限制
     
     @SuppressWarnings("unchecked")
     public Object run() throws ZuulException {
@@ -28,29 +31,24 @@ public class MyZuulFilter extends ZuulFilter {
         }
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
-        //HttpServletResponse response = requestContext.getResponse();
-        //String servletPath = request.getServletPath();
-        //个人认为具体权限应该由每个微服务自己处理
-        String token = request.getHeader("token");
-        if(token==null||CommonConstant.EMPTY_VALUE.equals(token)) {
-            requestContext.setSendZuulResponse(false);
+        HttpServletResponse response = requestContext.getResponse();
+        String servletPath = request.getServletPath();
+        boolean pass = interfaceCurrentLimiting(String.join(CommonConstant.COMMA,servletPath,HttpUtil.getIpAddress(request)));
+        if(!pass){
             try {
-            	requestContext.getResponse().setContentType("application/json;charset=UTF-8");
-                requestContext.setResponseBody(new ObjectMapper().writeValueAsString(new BaseResponseResult(HttpCodeEnum.VALIDATE_ERROR.getCode(),"token错误或不存在",CommonConstant.EMPTY_VALUE)));
-            } catch (JsonProcessingException e) {
+            	/**
+            	response.setContentType("application/json;charset=UTF-8");
+                requestContext.setResponseBody(new ObjectMapper().writeValueAsString(new BaseResponseResult(500,"错误",null)));
+                */
+                response.reset();
+                ServletOutputStream sos = response.getOutputStream();
+                response.setCharacterEncoding("UTF-8");          
+                response.setContentType("application/json;charset=UTF-8");   
+                sos.write("接口调用次数太频繁，请5分钟后再次尝试！".getBytes("UTF-8"));
+                sos.flush();
+                sos.close();
+            } catch (IOException e) {
                 //do nothing
-            }
-        }else {
-            //redis在这里有个坑，一个项目中对实体类序列化后，另一个项目读取该类如果两个项目的类的路径不一致就会报错
-            TokenData tokenData = (TokenData)(redisTemplate.opsForValue().get(token));
-            if(tokenData==null) {
-                requestContext.setSendZuulResponse(false);
-                try {
-                	requestContext.getResponse().setContentType("application/json;charset=UTF-8");
-                    requestContext.setResponseBody(new ObjectMapper().writeValueAsString(new BaseResponseResult(HttpCodeEnum.VALIDATE_ERROR.getCode(),"token错误或失效",CommonConstant.EMPTY_VALUE)));
-                } catch (JsonProcessingException e) {
-                    //do nothing
-                }
             }
         }
         return null;
@@ -68,11 +66,7 @@ public class MyZuulFilter extends ZuulFilter {
         return 0;
     }
     
-    /**
-    private RedisTemplate<String,Object> redisTemplate = null;
-    public static final int COUNT_TIMES = 3;//3次次数限制
-    public static final int LIMIT_TIME = 5;//5秒时间限制
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public boolean interfaceCurrentLimiting(String pathAndIp) {//简单接口限流
     	if(redisTemplate==null){
 			redisTemplate = (RedisTemplate<String,Object>)ApplicationContextHelper.getBean("redisTemplate");
@@ -90,23 +84,5 @@ public class MyZuulFilter extends ZuulFilter {
     	}
     	return true;
     }
-    */
-    
-    /**
-    boolean pass = interfaceCurrentLimiting(servletPath+","+request.getRemoteHost());
-    if(!pass){
-        try {
-            response.reset();
-            ServletOutputStream sos = response.getOutputStream();
-            response.setCharacterEncoding("UTF-8");          
-            response.setContentType("application/json;charset=UTF-8");   
-            sos.write("接口调用次数太频繁，请5分钟后再次尝试！".getBytes("UTF-8"));
-            sos.flush();
-            sos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    */
     
 }
