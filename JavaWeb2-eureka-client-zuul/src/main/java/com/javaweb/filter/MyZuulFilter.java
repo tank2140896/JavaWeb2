df70a4d929a1d3d195968a7968ffda78218af1d8
MrRiver <411833172@qq.com>
2020-04-16 14:58:28 +0800

    Update MyZuulFilter.java

--- JavaWeb2-eureka-client-zuul/src/main/java/com/javaweb/filter/MyZuulFilter.java
+++ JavaWeb2-eureka-client-zuul/src/main/java/com/javaweb/filter/MyZuulFilter.java
@@ -68,4 +68,45 @@ public class MyZuulFilter extends ZuulFilter {
         return 0;
     }
     
+    /**
+    private RedisTemplate<String,Object> redisTemplate = null;
+    public static final int COUNT_TIMES = 3;//3次次数限制
+    public static final int LIMIT_TIME = 5;//5秒时间限制
+    @SuppressWarnings("unchecked")
+	public boolean interfaceCurrentLimiting(String pathAndIp) {//简单接口限流
+    	if(redisTemplate==null){
+			redisTemplate = (RedisTemplate<String,Object>)ApplicationContextHelper.getBean("redisTemplate");
+		}
+    	if(redisTemplate.hasKey(pathAndIp)){//记录过
+    		int count = (int)redisTemplate.opsForValue().get(pathAndIp);
+    		if(count>=COUNT_TIMES){
+    			redisTemplate.opsForValue().set(pathAndIp,COUNT_TIMES+1,5,TimeUnit.MINUTES);//频繁调用封禁5分钟
+                return false;
+    		}else{
+    			redisTemplate.opsForValue().increment(pathAndIp,1);
+    		}
+    	}else{//没有记录过
+    		redisTemplate.opsForValue().set(pathAndIp,1,LIMIT_TIME,TimeUnit.SECONDS);
+    	}
+    	return true;
+    }
+    */
+    
+    /**
+    boolean pass = interfaceCurrentLimiting(servletPath+","+request.getRemoteHost());
+    if(!pass){
+        try {
+            response.reset();
+            ServletOutputStream sos = response.getOutputStream();
+            response.setCharacterEncoding("UTF-8");          
+            response.setContentType("application/json;charset=UTF-8");   
+            sos.write("接口调用次数太频繁，请5分钟后再次尝试！".getBytes("UTF-8"));
+            sos.flush();
+            sos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+    
 }
