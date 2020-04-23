deabb90d073d647843d8e4291d7304f0e227c59d
MrRiver <411833172@qq.com>
2020-04-23 15:48:15 +0800

    Update BaseService.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseService.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseService.java
@@ -3,14 +3,17 @@ package com.javaweb.base;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.javaweb.web.dao.ds1.CommonDao;
+import com.javaweb.web.dao.ds1.DataPermissionDao;
 import com.javaweb.web.dao.ds1.DictionaryDao;
 import com.javaweb.web.dao.ds1.InterfacesDao;
 import com.javaweb.web.dao.ds1.ModuleDao;
 import com.javaweb.web.dao.ds1.OperationLogDao;
 import com.javaweb.web.dao.ds1.RoleDao;
+import com.javaweb.web.dao.ds1.RoleDataDao;
 import com.javaweb.web.dao.ds1.RoleModuleDao;
 import com.javaweb.web.dao.ds1.ScheduleDao;
 import com.javaweb.web.dao.ds1.UserDao;
+import com.javaweb.web.dao.ds1.UserDataDao;
 import com.javaweb.web.dao.ds1.UserModuleDao;
 import com.javaweb.web.dao.ds1.UserRoleDao;
 
@@ -48,5 +51,14 @@ public class BaseService extends BaseTool {
 	
 	@Autowired
 	protected InterfacesDao interfacesDao;
+	
+	@Autowired
+	protected DataPermissionDao dataPermissionDao;
+	
+	@Autowired
+	protected UserDataDao userDataDao;
+	
+	@Autowired
+	protected RoleDataDao roleDataDao;
 
 }
