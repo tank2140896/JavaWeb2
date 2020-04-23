3e7de80ef6325752297e89962cfdf2fdda735274
MrRiver <411833172@qq.com>
2020-04-23 15:56:25 +0800

    Create UserRolePermissionResponse.java

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/interfaces/UserRolePermissionResponse.java
@@ -0,0 +1,19 @@
+package com.javaweb.web.eo.interfaces;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRolePermissionResponse implements Serializable {
+
+	private static final long serialVersionUID = 8502645489040187193L;
+
+	private List<UserPermissionResponse> userPermissionResponseList;
+	
+	private List<RolePermissionResponse> rolePermissionResponseList;
+
+}
