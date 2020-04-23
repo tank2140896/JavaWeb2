de5a2ca11a5c002698d9abc2eb24782888bec421
MrRiver <411833172@qq.com>
2020-04-23 15:37:27 +0800

    Create RoleData.java

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/po/RoleData.java
@@ -0,0 +1,27 @@
+package com.javaweb.web.po;
+
+import java.io.Serializable;
+
+import com.javaweb.annotation.sql.Column;
+import com.javaweb.annotation.sql.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name="role_data")
+public class RoleData implements Serializable {
+
+	private static final long serialVersionUID = -8720034407531582856L;
+
+	@Column(name="id",pk=true)
+	private String id;//主键ID
+	
+	@Column(name="role_id")
+	private String roleId;//角色ID
+
+	@Column(name="data_permission_id")
+	private String dataPermissionId;//数据权限ID
+	
+}
