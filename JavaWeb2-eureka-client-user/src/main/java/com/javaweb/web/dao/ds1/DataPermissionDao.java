79443a59c452d448494b824c578c5d09505f16ee
MrRiver <411833172@qq.com>
2020-04-24 10:24:08 +0800

    Update DataPermissionDao.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/dao/ds1/DataPermissionDao.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/dao/ds1/DataPermissionDao.java
@@ -1,11 +1,16 @@
 package com.javaweb.web.dao.ds1;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.javaweb.mybatis.api.DaoForMySql;
+import com.javaweb.web.eo.interfaces.ExcludeInfoResponse;
 import com.javaweb.web.po.DataPermission;
 
 @Mapper
 public interface DataPermissionDao extends DaoForMySql<DataPermission> {
+	
+	public List<ExcludeInfoResponse> selectExcludeInfo(List<String> list);
 	
 }
