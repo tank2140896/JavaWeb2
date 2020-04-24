d55075e2b607ece2007a91e2ef53e50e5afe93ba
MrRiver <411833172@qq.com>
2020-04-24 10:22:28 +0800

    Update InterfacesService.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/InterfacesService.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/InterfacesService.java
@@ -1,6 +1,9 @@
 package com.javaweb.web.service;
 
+import java.util.List;
+
 import com.javaweb.util.entity.Page;
+import com.javaweb.web.eo.interfaces.ExcludeInfoResponse;
 import com.javaweb.web.eo.interfaces.InterfacesListRequest;
 import com.javaweb.web.eo.interfaces.UserRolePermissionResponse;
 import com.javaweb.web.po.Interfaces;
@@ -18,5 +21,7 @@ public interface InterfacesService {
 	public UserRolePermissionResponse userRoleDataPermission(String interfacesId);
 	
 	public void dataPermissionAssignment(UserRolePermissionResponse userRolePermissionResponse,String interfacesId);
+	
+	public List<ExcludeInfoResponse> getExcludeInfoResponseList(String userId);
 
 }
