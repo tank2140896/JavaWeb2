01b6d3018be2425c60fb841410484f8cced2cf6c
MrRiver <411833172@qq.com>
2020-04-24 10:27:30 +0800

    Create ExcludeInfoResponse.java

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/interfaces/ExcludeInfoResponse.java
@@ -0,0 +1,19 @@
+package com.javaweb.web.eo.interfaces;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcludeInfoResponse implements Serializable {
+	
+	private static final long serialVersionUID = 3136601161004663224L;
+
+	private String excludeField;
+	
+	private String url;
+	
+
+}
