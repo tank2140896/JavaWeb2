b1f0b3e67935ed8845505cbd95b178de1be33eaa
MrRiver <411833172@qq.com>
2020-04-27 10:59:12 +0800

    Update online.chat.component.ts

--- JavaWeb2-angular/src/app/fun/onlineChat/online.chat.component.ts
+++ JavaWeb2-angular/src/app/fun/onlineChat/online.chat.component.ts
@@ -18,23 +18,16 @@ export class OnlineChatComponent implements OnInit {
               public sessionService:SessionService,
               public authService:AuthService,
               public router:Router) {
-    this.userOfflineZone = authService.canShow(ApiConstant.getPath(ApiConstant.SYS_ONLINE_CHAT_USER_OFFLINE,false));
   }
-
-  /** 操作权限 start */
-  userOfflineZone:boolean;//用户下线
-  /** 操作权限 end */
 
   private webSocket:WebSocket;
   private sendMessage:string;
   private getMessage:string  = CommonConstant.EMPTY;
   private userName:string;
-  private onLineUserList:any;
 
   ngOnInit() {
     this.webSocketConnect();
     this.userName = this.sessionService.getTokenData().user.userName;
-    this.onlineUseList();
   }
 
   //websocket连接
@@ -82,44 +75,6 @@ export class OnlineChatComponent implements OnInit {
   public keydownEnter($event):void {
     //console.log($event);
     this.sendFunction();
-  }
-
-  //获得在线用户列表
-  public onlineUseList():void {
-    this.httpService.getJsonData(ApiConstant.getPath(ApiConstant.SYS_ONLINE_CHAT_USER_LIST,true),this.sessionService.getHeadToken()).subscribe(
-      {
-        next:(result:any) => {
-          //console.log(result);
-          if(result.code==200){
-            let ret = result.data;
-            this.onLineUserList = ret;
-          }else{
-            this.router.navigate(['webLogin']);
-          }
-        },
-        error:e => {},
-        complete:() => {}
-      }
-    );
-  }
-
-  //用户下线
-  public userOffline(eachData):void {
-    this.httpService.getJsonData(ApiConstant.getPath(ApiConstant.SYS_ONLINE_CHAT_USER_OFFLINE+'/'+eachData.userId+',1',true),this.sessionService.getHeadToken()).subscribe(
-      {
-        next:(result:any) => {
-          //console.log(result);
-          if(result.code==200){
-            alert('用户['+eachData.userName+']成功下线');
-            this.onlineUseList();
-          }else{
-            this.router.navigate(['webLogin']);
-          }
-        },
-        error:e => {},
-        complete:() => {}
-      }
-    );
   }
 
 }
