3dec8edd39e4a40805285b7820961bef123328f7
MrRiver <411833172@qq.com>
2020-04-27 10:40:47 +0800

    Update OnlineUserController.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/controller/OnlineUserController.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/controller/OnlineUserController.java
@@ -1,11 +1,15 @@
 package com.javaweb.web.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,45 +18,53 @@ import com.javaweb.base.BaseResponseResult;
 import com.javaweb.constant.ApiConstant;
 import com.javaweb.constant.CommonConstant;
 import com.javaweb.constant.SwaggerConstant;
-import com.javaweb.constant.SystemConstant;
 import com.javaweb.enums.HttpCodeEnum;
+import com.javaweb.util.core.PageUtil;
+import com.javaweb.util.core.StringUtil;
+import com.javaweb.util.entity.Page;
+import com.javaweb.web.eo.onlineUser.OnlineUserListRequest;
 import com.javaweb.web.po.User;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(tags=SwaggerConstant.SWAGGER_ONLINE_CHAT_CONTROLLER_TAGS)
+@Api(tags=SwaggerConstant.SWAGGER_ONLINE_USER_CONTROLLER_TAGS)
 @RestController
-@RequestMapping(ApiConstant.WEB_ONLINE_CHAT_PREFIX)
-public class OnlineChatController extends BaseController {
+@RequestMapping(ApiConstant.WEB_ONLINE_USER_PREFIX)
+public class OnlineUserController extends BaseController {
 	
-	@ApiOperation(value=SwaggerConstant.SWAGGER_ONLINE_CHAT_USER_LIST)
-	@GetMapping(ApiConstant.ONLINE_CHAT_USER_LIST)
-	public BaseResponseResult userList(){
-		Set<String> set = stringRedisTemplate.keys("*,1");//只取web端的在线用户，这里做的较简单，全部取出不分页
-		/**
-		数组手动分页处理方式：
-		1、List<String> sortedList = set.stream().sorted().collect(Collectors.toList());
-		2、调用PageUtil.getSubList(list,pageSize,currentPage)
-		*/
-		List<String> noSortist = new ArrayList<>();
+	@ApiOperation(value=SwaggerConstant.SWAGGER_ONLINE_USER_LIST)
+	@PostMapping(ApiConstant.ONLINE_USER_LIST)
+	public BaseResponseResult list(@RequestBody OnlineUserListRequest onlineUserListRequest){
+		Set<String> set = stringRedisTemplate.keys("*,1");//只取web端的在线用户
+		set = (set==null?new HashSet<>():set);
+		List<String> sortedList = new ArrayList<>();
 		for(String str:set){
-			if(!str.equals(SystemConstant.ADMIN+CommonConstant.COMMA+CommonConstant.ZERO_STRING_VALUE)){//去除管理员
-				noSortist.add(str.split(CommonConstant.COMMA)[0]);
-			}
+			sortedList.add(str.split(CommonConstant.COMMA)[0]);
+		}
+		String userName = onlineUserListRequest.getUserName();
+		if(StringUtil.handleNullString(userName).trim().equals(CommonConstant.EMPTY_VALUE)){
+			sortedList = sortedList.stream().sorted().collect(Collectors.toList());
+		}else{
+			sortedList = sortedList.stream().filter(e->e.contains(onlineUserListRequest.getUserName())).sorted().collect(Collectors.toList());
+		}
+		sortedList = PageUtil.getSubList(sortedList,onlineUserListRequest.getPageSize(),onlineUserListRequest.getCurrentPage());
+		if(sortedList==null){
+			sortedList = new ArrayList<>();
 		}
 		List<User> list = new ArrayList<>();
-		if(noSortist.size()!=0){
-			list = userService.getUsersByUserId(noSortist);
+		if(sortedList.size()!=0){
+			list = userService.getUsersByUserId(sortedList);
 		}
-		return getBaseResponseResult(HttpCodeEnum.SUCCESS,"onlineChat.userList.success",list);
+		Page page = new Page(onlineUserListRequest,list,(set.size()+0L));
+		return getBaseResponseResult(HttpCodeEnum.SUCCESS,"onlineUser.list.success",page);
 	}
 	
-	@ApiOperation(value=SwaggerConstant.SWAGGER_ONLINE_CHAT_USER_OFFLINE)
-	@GetMapping(ApiConstant.ONLINE_CHAT_USER_OFFLINE)
-	public BaseResponseResult userOffline(@PathVariable(name="key",required=true) String key){
+	@ApiOperation(value=SwaggerConstant.SWAGGER_ONLINE_USER_OFFLINE)
+	@GetMapping(ApiConstant.ONLINE_USER_OFFLINE)
+	public BaseResponseResult offline(@PathVariable(name="key",required=true) String key){
 		stringRedisTemplate.delete(key);
-		return getBaseResponseResult(HttpCodeEnum.SUCCESS,"onlineChat.userOffline.success",null);
+		return getBaseResponseResult(HttpCodeEnum.SUCCESS,"onlineUser.offline.success",null);
 	}
 
 }
