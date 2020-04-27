101fca882e4edc9dd1f4e138e29fa11d5cb146f8
MrRiver <411833172@qq.com>
2020-04-27 12:01:11 +0800

    Update online.user.component.ts

--- JavaWeb2-angular/src/app/fun/onlineUser/online.user.component.ts
+++ JavaWeb2-angular/src/app/fun/onlineUser/online.user.component.ts
@@ -5,7 +5,7 @@ import {Router} from '@angular/router';
 import {ApiConstant} from '../../constant/ApiConstant';
 import {AuthService} from '../../service/AuthService';
 import {ResultPage} from '../../model/ResultPage';
-import {OnlineUserListRequest} from '../../model/onlineUser/OnlineUserRequest';
+import {OnlineUserListRequest} from '../../model/onlineUser/OnlineUserListRequest';
 
 @Component({
   selector: 'app-web-online-user',
