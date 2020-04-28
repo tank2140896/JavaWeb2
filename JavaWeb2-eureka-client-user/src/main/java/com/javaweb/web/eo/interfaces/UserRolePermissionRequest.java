5bca6647af50e453b92ee3cc6f0cd59dee097e28
MrRiver <411833172@qq.com>
2020-04-28 10:50:12 +0800

    Update UserRolePermissionRequest.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/interfaces/UserRolePermissionRequest.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/interfaces/UserRolePermissionRequest.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UserRolePermissionResponse implements Serializable {
+public class UserRolePermissionRequest implements Serializable {
 
 	private static final long serialVersionUID = 8502645489040187193L;
 
