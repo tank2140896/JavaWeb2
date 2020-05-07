9ed09f8cb61536cccd517450acab7e52305ccf80
MrRiver <411833172@qq.com>
2020-05-07 14:54:32 +0800

    Update DbTablesService.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/DbTablesService.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/DbTablesService.java
@@ -1,10 +1,15 @@
 package com.javaweb.web.service;
 
+import java.util.List;
+
 import com.javaweb.util.entity.Page;
+import com.javaweb.web.eo.dbTables.DbTablesColumnListResponse;
 import com.javaweb.web.eo.dbTables.DbTablesListRequest;
 
 public interface DbTablesService {
 	
 	public Page dbTablesList(DbTablesListRequest dbTablesListRequest);
+	
+	public List<DbTablesColumnListResponse> getTableColumnInfo(String tableName);
 	
 }
