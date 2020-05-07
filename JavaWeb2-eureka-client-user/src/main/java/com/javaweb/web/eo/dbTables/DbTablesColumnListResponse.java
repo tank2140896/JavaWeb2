39c5bca5eeddbd0dba8ae942f4b4467eb03cfb47
MrRiver <411833172@qq.com>
2020-05-07 14:57:44 +0800

    Create DbTablesColumnListResponse.java

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/dbTables/DbTablesColumnListResponse.java
@@ -0,0 +1,26 @@
+package com.javaweb.web.eo.dbTables;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DbTablesColumnListResponse implements Serializable {
+
+	private static final long serialVersionUID = 982904744299610555L;
+	
+	private String field;//字段名称
+	
+	private String type;//字段类型
+	
+	private String isNull;//是否是null
+	
+	private String isKey;//是否是主键
+	
+	private String defaultValue;//默认值
+	
+	private String extra;//额外说明
+
+}
