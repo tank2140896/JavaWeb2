be0475ece00b257849c0140bc691f75660929e6d
MrRiver <411833172@qq.com>
2020-05-07 10:52:08 +0800

    Update DbTablesServiceImpl.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/impl/DbTablesServiceImpl.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/impl/DbTablesServiceImpl.java
@@ -1,5 +1,6 @@
 package com.javaweb.web.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ import com.javaweb.util.core.PageUtil;
 import com.javaweb.util.core.StringUtil;
 import com.javaweb.util.entity.Page;
 import com.javaweb.web.eo.dbTables.DbTablesListRequest;
+import com.javaweb.web.eo.dbTables.DbTablesListResponse;
 import com.javaweb.web.service.DbTablesService;
 
 @Service("dbTablesServiceImpl")
@@ -23,7 +25,11 @@ public class DbTablesServiceImpl extends BaseService implements DbTablesService 
 		}
 		long count = tablelist.size();
 		tablelist = PageUtil.getSubList(tablelist,dbTablesListRequest.getPageSize(),dbTablesListRequest.getCurrentPage());
-		Page page = new Page(dbTablesListRequest,tablelist,count);
+		List<DbTablesListResponse> list = new ArrayList<>();
+		if(tablelist!=null&&tablelist.size()>0){
+			list = dbTablesDao.getTableInfo(tablelist);
+		}
+		Page page = new Page(dbTablesListRequest,list,count);
 		return page;
 	}
 	
