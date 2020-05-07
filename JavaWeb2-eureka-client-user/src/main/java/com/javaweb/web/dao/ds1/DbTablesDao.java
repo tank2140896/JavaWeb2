dc28e88c663531fab2cd64dc26643ca4d9297c9a
MrRiver <411833172@qq.com>
2020-05-07 10:49:38 +0800

    Update DbTablesDao.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/dao/ds1/DbTablesDao.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/dao/ds1/DbTablesDao.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.javaweb.web.eo.dbTables.DbTablesListResponse;
+
 @Mapper
 public interface DbTablesDao {
 	
 	public List<String> getTableList();
+	
+	public List<DbTablesListResponse> getTableInfo(List<String> list);
 
 }
