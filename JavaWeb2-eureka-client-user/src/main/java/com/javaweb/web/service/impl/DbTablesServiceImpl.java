2ec4b0ed323543a99d12039dc4910ded9542ec62
MrRiver <411833172@qq.com>
2020-05-07 14:55:12 +0800

    Update DbTablesServiceImpl.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/impl/DbTablesServiceImpl.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/impl/DbTablesServiceImpl.java
@@ -1,7 +1,9 @@
 package com.javaweb.web.service.impl;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import com.javaweb.constant.CommonConstant;
 import com.javaweb.util.core.PageUtil;
 import com.javaweb.util.core.StringUtil;
 import com.javaweb.util.entity.Page;
+import com.javaweb.web.eo.dbTables.DbTablesColumnListResponse;
 import com.javaweb.web.eo.dbTables.DbTablesListRequest;
 import com.javaweb.web.eo.dbTables.DbTablesListResponse;
 import com.javaweb.web.service.DbTablesService;
@@ -27,10 +30,31 @@ public class DbTablesServiceImpl extends BaseService implements DbTablesService 
 		tablelist = PageUtil.getSubList(tablelist,dbTablesListRequest.getPageSize(),dbTablesListRequest.getCurrentPage());
 		List<DbTablesListResponse> list = new ArrayList<>();
 		if(tablelist!=null&&tablelist.size()>0){
-			list = dbTablesDao.getTableInfo(tablelist);
+			list = dbTablesDao.getTableInfo(tablelist).stream().sorted(new Comparator<DbTablesListResponse>() {
+				public int compare(DbTablesListResponse o1, DbTablesListResponse o2) {
+					return o1.getTableName().hashCode()>o2.getTableName().hashCode()?0:1;
+				}
+			}).collect(Collectors.toList());
 		}
 		Page page = new Page(dbTablesListRequest,list,count);
 		return page;
+	}
+	
+	public List<DbTablesColumnListResponse> getTableColumnInfo(String tableName) {
+		List<DbTablesColumnListResponse> result = new ArrayList<>();
+		List<Map<String,Object>> list = dbTablesDao.getTableColumnInfo(tableName);
+		for(int i=0;i<list.size();i++){
+			Map<String,Object> map = list.get(i);
+			DbTablesColumnListResponse dbTablesColumnListResponse = new DbTablesColumnListResponse();
+			dbTablesColumnListResponse.setField(map.get("Field").toString());
+			dbTablesColumnListResponse.setType(map.get("Type")==null?null:String.valueOf(map.get("Type")));
+			dbTablesColumnListResponse.setIsNull(map.get("Null")==null?null:String.valueOf(map.get("Null")));
+			dbTablesColumnListResponse.setExtra(map.get("Extra")==null?null:String.valueOf(map.get("Extra")));
+			dbTablesColumnListResponse.setDefaultValue(map.get("Default")==null?null:String.valueOf(map.get("Default")));
+			dbTablesColumnListResponse.setIsKey(map.get("Key")==null?null:String.valueOf(map.get("Key")));
+			result.add(dbTablesColumnListResponse);
+		}
+		return result;
 	}
 	
 }
