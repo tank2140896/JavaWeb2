d4fffa0d35478d54a9c7ca5e8c6ea4bac60f9550
tank2140896 <411833172@qq.com>
2020-05-09 19:27:33 +0800

    no message

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaConfig.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaConfig.java
@@ -15,17 +15,17 @@ public class KaptchaConfig {
     public DefaultKaptcha getDefaultKaptcha(){  
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();  
         Properties properties = new Properties();  
-        properties.setProperty("kaptcha.border", "yes");  
-        properties.setProperty("kaptcha.border.color", "105,179,90");  
-        properties.setProperty("kaptcha.textproducer.font.color", "red");  
-        properties.setProperty("kaptcha.image.width", "100");  
-        properties.setProperty("kaptcha.image.height", "27");  
-        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise"); 
-        properties.setProperty("kaptcha.textproducer.font.size", "26");  
-        properties.setProperty("kaptcha.textproducer.char.length", "4");  
-        properties.setProperty("kaptcha.textproducer.char.space", "3");  
-        properties.setProperty("kaptcha.textproducer.char.string", "A3C4EFGH6JK7MN8PR9TWX");  
-        properties.setProperty("kaptcha.textproducer.font.names", "Arial");  
+        properties.setProperty("kaptcha.border","yes");  
+        properties.setProperty("kaptcha.border.color","105,179,90");  
+        properties.setProperty("kaptcha.textproducer.font.color","red");  
+        properties.setProperty("kaptcha.image.width","100");  
+        properties.setProperty("kaptcha.image.height","27");  
+        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise"); 
+        properties.setProperty("kaptcha.textproducer.font.size","26");  
+        properties.setProperty("kaptcha.textproducer.char.length","4");  
+        properties.setProperty("kaptcha.textproducer.char.space","3");  
+        properties.setProperty("kaptcha.textproducer.char.string","A3C4EFGH6JK7MN8PR9TWX");  
+        properties.setProperty("kaptcha.textproducer.font.names","Arial");  
         Config config = new Config(properties);  
         defaultKaptcha.setConfig(config);  
         return defaultKaptcha;  
--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaController.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaController.java
@@ -1,8 +1,6 @@
 package com.javaweb.config.kaptcha;
 
 import java.awt.image.BufferedImage;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
@@ -10,40 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.base.BaseInject;
+import com.javaweb.util.core.HttpUtil;
 
 @RestController
 public class KaptchaController extends BaseInject {
 	
-	@GetMapping("/getRequestId")
-	public String getRequestId() {
-		Map<String,Object> map = new HashMap<>();
-		map.put("key",defaultKaptcha.createText());
-		map.put("date","yyyyMMddHHmmss");
-		return "success";
-	}
-	
+	//获取验证码
 	@GetMapping("getKaptcha")
-	public void getKaptcha(HttpServletRequest request,HttpServletResponse response,@PathVariable(name="requestId",required=true) String requestId) throws Exception {
-		response.setHeader("Cache-Control","no-store,no-cache");
-	    response.setContentType("image/jpeg");
-	    String text = defaultKaptcha.createText();
-	    kaptchaCheck(request);//这里可以进行验证码的校验
-	    BufferedImage image = defaultKaptcha.createImage(text);
-	    ServletOutputStream out = response.getOutputStream();
-	    ImageIO.write(image,"jpg",out);
+	public void getKaptcha(HttpServletRequest request,HttpServletResponse response) {
+		try{
+			response.setHeader("Cache-Control","no-store,no-cache");
+		    response.setContentType("image/jpeg");
+		    String ip = HttpUtil.getIpAddress(request);
+		    String text = defaultKaptcha.createText();
+		    stringRedisTemplate1.opsForValue().set(ip,text);
+		    BufferedImage image = defaultKaptcha.createImage(text);
+		    ServletOutputStream out = response.getOutputStream();
+		    ImageIO.write(image,"jpg",out);
+		}catch(Exception e){
+			//do nothing
+		}
 	}
 	
 	//验证码校验
-	private boolean kaptchaCheck(/*UserLoginRequest userLogin,*/HttpServletRequest request){
+	public boolean kaptchaCheck(String kaptchaFromFrontend,HttpServletRequest request){
+		String ip = HttpUtil.getIpAddress(request);
 		boolean result = true;
 		//可以从redis中获取
-		String kaptcha = "get from redis";//(String)getDateFromRedis(userLogin.getRequestId());
+		String kaptcha = stringRedisTemplate1.opsForValue().get(ip);
 		if(kaptcha!=null){
-			if(kaptcha.equalsIgnoreCase("获得到的验证码"/*userLogin.getKaptcha()*/)){//忽略大小写
+			if(kaptcha.equalsIgnoreCase(kaptchaFromFrontend)){//忽略大小写
 				result = false;
 			}
 		}
