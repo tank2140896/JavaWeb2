e4c56dd3204a2ed5c185c8eac30e4cce281d176b
MrRiver <411833172@qq.com>
2020-05-09 13:47:32 +0800

    Update AppModule.ts

--- JavaWeb2-angular/src/app/AppModule.ts
+++ JavaWeb2-angular/src/app/AppModule.ts
@@ -4,7 +4,7 @@ import {AppRoutingModule} from './AppRoutingModule';
 import {AppComponent} from './AppComponent';
 import {LoginModule} from './fun/login/login.module';
 import {HttpService} from './service/HttpService';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {SessionService} from './service/SessionService';
 import {AuthService} from './service/AuthService';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -12,6 +12,12 @@ import {CommonModule} from '@angular/common';
 import {HomeModule} from './fun/home/home.module';
 import {FormsModule} from '@angular/forms';
 import {DemoModule} from './fun/demo/demo.module';
+import {HttpServiceInterceptor} from './interceptor/HttpServiceInterceptor';
+
+//可以写入多个拦截器
+export const InterceptorProviders = [
+  {provide:HTTP_INTERCEPTORS,useClass:HttpServiceInterceptor,multi:true},
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +28,9 @@ import {DemoModule} from './fun/demo/demo.module';
     AppRoutingModule,
     LoginModule,HomeModule,DemoModule
   ],
-  providers: [HttpService,SessionService,AuthService],
+  providers: [HttpService,SessionService,AuthService,InterceptorProviders],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
