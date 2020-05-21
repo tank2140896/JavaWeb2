7bfa41c0709a64d216588e5d30fc4a2777c73137
MrRiver <411833172@qq.com>
2020-05-21 15:00:22 +0800

    Update TokenDataHandlerMethodArgumentResolver.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/resolver/TokenDataHandlerMethodArgumentResolver.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/resolver/TokenDataHandlerMethodArgumentResolver.java
@@ -1,7 +1,6 @@
 package com.javaweb.resolver;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -9,26 +8,18 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import com.javaweb.annotation.token.TokenDataAnnotation;
 import com.javaweb.base.BaseTool;
-import com.javaweb.constant.SystemConstant;
-import com.javaweb.context.ApplicationContextHelper;
 import com.javaweb.exception.TokenExpiredException;
 import com.javaweb.web.eo.TokenData;
 
 public class TokenDataHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 	
-	private RedisTemplate<String,Object> redisTemplate = null;
-
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().isAssignableFrom(TokenData.class) && parameter.hasParameterAnnotation(TokenDataAnnotation.class);
     }
     
     //这里不建议将NativeWebRequest转为HttpServeltRequest
-    @SuppressWarnings("unchecked")
 	public Object resolveArgument(MethodParameter parameter,ModelAndViewContainer container,NativeWebRequest request,WebDataBinderFactory factory) throws Exception {
-		if(redisTemplate==null){
-			redisTemplate = (RedisTemplate<String,Object>)ApplicationContextHelper.getBean(SystemConstant.REDIS_TEMPLATE);
-		}
-		TokenData tokenData = BaseTool.getTokenData(BaseTool.getToken(request),redisTemplate);
+		TokenData tokenData = BaseTool.getTokenData(BaseTool.getToken(request));
 		if(tokenData==null){
 			throw new TokenExpiredException();
 		}
