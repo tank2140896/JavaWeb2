2947b6a18cb7c123ddbab1b04d8d3bf62b9f4165
MrRiver <411833172@qq.com>
2020-05-22 11:36:49 +0800

    Update KaptchaConfig.java

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaConfig.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/kaptcha/KaptchaConfig.java
@@ -18,18 +18,18 @@ public class KaptchaConfig {
         properties.setProperty("kaptcha.border","yes");  
         properties.setProperty("kaptcha.border.color","105,179,90");  
         properties.setProperty("kaptcha.textproducer.font.color","blue");
-        properties.setProperty("kaptcha.image.width","100");  
-        properties.setProperty("kaptcha.image.height","27");  
-        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise");
-        properties.setProperty("kaptcha.obscurificator.impl","com.google.code.kaptcha.impl.ShadowGimpy");
-        properties.setProperty("kaptcha.textproducer.font.size","26");  
+        properties.setProperty("kaptcha.image.width","300");
+        properties.setProperty("kaptcha.image.height","75");
+        properties.setProperty("kaptcha.noise.impl","com.cpic.config.kaptcha.SelfNoise");//干扰 com.google.code.kaptcha.impl.NoNoise为无干扰，此处用的selfNoise是自己实现的干扰配置
+        //properties.setProperty("kaptcha.obscurificator.impl","com.google.code.kaptcha.impl.ShadowGimpy");
+        properties.setProperty("kaptcha.textproducer.font.size","70");
         properties.setProperty("kaptcha.textproducer.char.length","4");  
-        properties.setProperty("kaptcha.textproducer.char.space","3");  
-        properties.setProperty("kaptcha.textproducer.char.string","A3C4EFGH6JK7MN8PR9TWX");  
-        properties.setProperty("kaptcha.textproducer.font.names","Helvetica");
+        properties.setProperty("kaptcha.textproducer.char.space","10");
+        properties.setProperty("kaptcha.textproducer.char.string","AB3NP8RC4EF6HKMU2WXY");  
+        properties.setProperty("kaptcha.textproducer.font.names","宋体,楷体,微软雅黑");
         Config config = new Config(properties);  
         defaultKaptcha.setConfig(config);  
         return defaultKaptcha;  
-    }  
+    } 
 
 }
