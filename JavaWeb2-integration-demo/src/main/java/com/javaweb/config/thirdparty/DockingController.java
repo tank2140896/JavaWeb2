ecfce36b4d8a1e867f94f08834af04a6ada0b9b8
tank2140896 <411833172@qq.com>
2020-05-27 20:13:27 +0800

    no message

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/thirdparty/DockingController.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/thirdparty/DockingController.java
@@ -14,16 +14,16 @@ import lombok.Setter;
 @RestController
 public class DockingController {
 
-	//step1:给予对方一个key和appId
+	//step1:给予对方一个key和RSA公钥
 	
-	//step2:通过key获取token
-	public String getTokenBykey(@RequestParam(value="key") String key,@RequestParam(value="appId") String appId){
-		//step2.1:可以做的全面点，存入表中，校验下给予的这个key是否正确、是否是指定的使用厂商、使用期限是否到期、是否被禁用等
-		//step2.2:将token存入如redis中，给其一个存续期
-		return "token";//这个token我们约定是一个3DES的key值
+	//step2:通过RSA公钥加密key获取token
+	public String getTokenBykey(@RequestParam(value="key") String key){//key是经过RSA公钥加密的
+		//step2.1:可以做的全面点，存入表中，校验下给予的这个key（没有经过RSA公钥加密）是否正确、是否是指定的使用厂商、使用期限是否到期、是否被禁用等
+		//step2.2:将针对key（没有经过RSA公钥加密）生成token+3DES的key并将token+3DES的key存入如redis中，给其一个存续期
+		return "token+3DES的key";
 	}
 	
-	//step3:请求方将body参数用3DES加密传给我们，我们解密并返回数据给请求方
+	//step3:请求方将body（带有token）参数用3DES加密传给我们，我们解密并返回数据给请求方
 	public String returnResult(@RequestBody GiveMeRequestBody giveMeRequestBody) throws Exception {
 		String code = giveMeRequestBody.getCode();
 		code = DesUtil.decrypt(code,null/*秘钥*/);
