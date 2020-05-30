339d8199f5db584b2ebac3bacf6f480c2ee2cc59
tank2140896 <411833172@qq.com>
2020-05-30 10:52:03 +0800

    no message

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/config/elasticsearch/ElasticsearchConfig.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/config/elasticsearch/ElasticsearchConfig.java
@@ -1,25 +1,26 @@
 package com.javaweb.config.elasticsearch;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
+/**
+windows下安装操作：
+下载地址：https://www.elastic.co/cn/downloads/
+分词插件（放入位置：elasticsearch下的plugins目录下）下载地址：https://github.com/medcl/elasticsearch-analysis-ik/releases
+参考文档：https://docs.spring.io/spring-data/elasticsearch/docs/4.0.0.RELEASE/reference/html/#reference
+数据采集传输可以使用：Logstash
+*/
 @Configuration
-public class ElasticsearchConfig {
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
-	@Bean(name="transportClient")
-	public TransportClient transportClient() throws UnknownHostException {
-		TransportAddress node = new TransportAddress(InetAddress.getByName("192.168.0.102"),9200);
-		Settings settings = Settings.builder().put("cluster.name","elasticsearch").build();
-		TransportClient transportClient = new PreBuiltTransportClient(settings);
-		transportClient.addTransportAddress(node);
-		return transportClient;
+	@Bean
+	public RestHighLevelClient elasticsearchClient() {
+		final ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("localhost:9200").build();
+        return RestClients.create(clientConfiguration).rest();
 	}
 
 }
