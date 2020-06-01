a76c08cbb470f2dfa5e2d31a61e402787bcf9529
MrRiver <411833172@qq.com>
2020-06-01 14:27:45 +0800

    Update BaseInject.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseInject.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseInject.java
@@ -1,11 +1,14 @@
 package com.javaweb.base;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.netflix.discovery.EurekaClient;
@@ -32,8 +35,13 @@ public class BaseInject {
 	@Qualifier("mysql_d2_JdbcTemplate")
 	protected JdbcTemplate mysql_d2_JdbcTemplate;
 	
+	/** 两种装配bean的方式  start */
 	@Autowired
 	@Qualifier("redisTemplate")
 	protected StringRedisTemplate stringRedisTemplate;
+	
+	@Resource(name="redisTemplate")
+	protected ValueOperations<Object,Object> valueOperations;
+	/** 两种装配bean的方式  end */
 	
 }
