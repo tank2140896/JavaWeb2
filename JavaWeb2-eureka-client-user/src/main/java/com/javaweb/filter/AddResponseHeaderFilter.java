5c0db474febf38414c26f51147b8415a1bb8f91d
MrRiver <411833172@qq.com>
2020-08-17 14:56:09 +0800

    Update AddResponseHeaderFilter.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/filter/AddResponseHeaderFilter.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/filter/AddResponseHeaderFilter.java
@@ -16,7 +16,7 @@ public class AddResponseHeaderFilter extends OncePerRequestFilter {
 	//x.y.z：x为主版本号；y为次版本号（功能）；z为修订号（bug修复）
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		response.addHeader("Api-Version","1.0.0 alpha-1889");
-        filterChain.doFilter(request, response);
+		filterChain.doFilter(request, response);
 	}
 
 }
