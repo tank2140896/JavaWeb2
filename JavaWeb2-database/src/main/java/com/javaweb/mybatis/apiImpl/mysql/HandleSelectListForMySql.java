d2d4c29e6dc8d89673b1ffe207eb3afa4fbb32f8
MrRiver <411833172@qq.com>
2020-09-02 09:47:24 +0800

    Create HandleSelectListForMySql.java

---
+++ JavaWeb2-database/src/main/java/com/javaweb/mybatis/apiImpl/mysql/HandleSelectListForMySql.java
@@ -0,0 +1,60 @@
+package com.javaweb.mybatis.apiImpl.mysql;
+
+import java.util.List;
+
+import com.javaweb.enums.SqlConditionEnum;
+import com.javaweb.mybatis.apiImpl.SqlBuildInfo;
+import com.javaweb.mybatis.apiImpl.SqlCondition;
+import com.javaweb.mybatis.apiImpl.SqlHandle;
+import com.javaweb.query.QueryWapper;
+
+public class HandleSelectListForMySql implements SqlHandle {
+
+	public String handle(SqlBuildInfo sqlBuildInfo) {
+		QueryWapper<?> queryWapper = (QueryWapper<?>)sqlBuildInfo.getParameterValue();
+		List<SqlCondition> sqlConditionList = queryWapper.getSqlConditionList();
+		String tableName = sqlBuildInfo.getTableName();
+		List<String> entityList = sqlBuildInfo.getEntityList();
+		List<String> columnList = sqlBuildInfo.getColumnList();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("select ");
+		for(int i=0;i<entityList.size();i++){
+			stringBuilder.append(columnList.get(i)).append(" as ").append(entityList.get(i));
+			if(i!=entityList.size()-1){
+				stringBuilder.append(",");
+			}
+		}
+		stringBuilder.append(" from ").append(tableName).append(" where 1=1 ");
+		if(sqlConditionList.size()>0){
+			for(int i=0;i<sqlConditionList.size();i++){
+				SqlCondition sqlCondition = sqlConditionList.get(i);
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.EQ)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" = ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+					break;
+				}
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LIKE)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" like ").append("'%"+sqlCondition.getColumnValue()+"%'");
+					break;
+				}
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LT)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" < ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+					break;
+				}
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LE)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" <= ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+					break;
+				}
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.GT)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" > ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+					break;
+				}
+				if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.GE)==0){
+					stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" >= ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+					break;
+				}
+			}
+		}
+		return stringBuilder.toString();
+	}
+
+}
