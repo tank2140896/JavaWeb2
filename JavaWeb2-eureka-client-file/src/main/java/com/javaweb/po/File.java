1a6cbf1990119aa9ca2379d640a4f11af8d7e68f
MrRiver <411833172@qq.com>
2020-11-03 15:31:25 +0800

    Update File.java

--- JavaWeb2-eureka-client-file/src/main/java/com/javaweb/po/File.java
+++ JavaWeb2-eureka-client-file/src/main/java/com/javaweb/po/File.java
@@ -14,39 +14,39 @@ import lombok.Setter;
 @Table(name="sys_file")
 public class File extends BaseEntity implements Serializable {
 	
-	private static final long serialVersionUID = 5918807982525592278L;
+	private static final long serialVersionUID = -7761314189545429432L;
 
-	@Column(name="file_id",pk=true)
-	private String fileId;//文件ID
+	@Column(name="id",pk=true,columnDesc="主键ID")
+	private String id;//主键ID
 	
-	@Column(name="user_id")
+	@Column(name="user_id",columnDesc="用户ID")
 	private String userId;//用户ID
 	
-	@Column(name="micro_sys_no")
+	@Column(name="micro_sys_no",columnDesc="微服务系统编号")
 	private String microSysNo;//微服务系统编号
 	
-	@Column(name="file_name")
+	@Column(name="file_name",columnDesc="文件名称")
 	private String fileName;//文件名称
 
-	@Column(name="file_path")
+	@Column(name="file_path",columnDesc="文件路径")
 	private String filePath;//文件路径
 	
-	@Column(name="file_full_path")
+	@Column(name="file_full_path",columnDesc="文件全路径")
 	private String fileFullPath;//文件全路径
 	
-	@Column(name="file_size")
+	@Column(name="file_size",columnDesc="文件大小")
 	private String fileSize;//文件大小
 	
-	@Column(name="file_unit")
+	@Column(name="file_unit",columnDesc="文件大小单位")
 	private String fileUnit;//文件大小单位
 	
-	@Column(name="file_ser_no")
+	@Column(name="file_ser_no",columnDesc="文件批次号")
 	private String fileSerNo;//文件批次号
 	
-	@Column(name="remark")
+	@Column(name="remark",columnDesc="备注")
 	private String remark;//备注
 	
-	@Column(name="status")
-	private Integer status = 0;//账号状态(0:正常；1：禁用)
+	@Column(name="status",columnDesc="状态(0:正常；1：禁用)")
+	private Integer status = 0;//状态(0:正常；1：禁用)
 
 }
