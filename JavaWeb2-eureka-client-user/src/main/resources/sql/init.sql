2eed19ef697e2e175231531988e2b16d555fc3d1
MrRiver <411833172@qq.com>
2020-11-03 16:46:44 +0800

    Update init.sql

--- JavaWeb2-eureka-client-user/src/main/resources/sql/init.sql
+++ JavaWeb2-eureka-client-user/src/main/resources/sql/init.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : test
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : test
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2020-11-03 16:45:27
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
