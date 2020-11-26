81f9a13ba9135fa7935896c89ae9af6ad486e2d5
MrRiver <411833172@qq.com>
2020-11-26 13:57:26 +0800

    Add files via upload

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseValidateService.java
@@ -0,0 +1,14 @@
+package com.javaweb.base;
+
+import java.util.Map;
+
+import com.javaweb.mybatis.api.DaoWapper;
+
+public interface BaseValidateService {
+	
+	//校验字段是否重复
+	public <T> BaseServiceValidateResult isColumnsRepeat(Map<String,Object> map,DaoWapper<T> daoWapper,String message);
+	
+	//可以追加其它自定义的业务校验
+
+}
---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/base/BaseValidateServiceImpl.java
@@ -0,0 +1,40 @@
+package com.javaweb.base;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.javaweb.constant.CommonConstant;
+import com.javaweb.mybatis.api.DaoWapper;
+import com.javaweb.query.QueryWapper;
+
+@Service("baseValidateServiceImpl")
+public class BaseValidateServiceImpl extends BaseService implements BaseValidateService {
+	
+	public <T> BaseServiceValidateResult isColumnsRepeat(Map<String,Object> map,DaoWapper<T> daoWapper,String message){
+		BaseServiceValidateResult baseServiceValidateResult = new BaseServiceValidateResult();
+		baseServiceValidateResult.setValidatePass(true);
+		String returnMessage = null;
+		if((message!=CommonConstant.NULL_VALUE)&&(!CommonConstant.EMPTY_VALUE.equals(message.trim()))){
+			try{returnMessage = getMessage(message);}catch(Exception e){}	
+		}
+		if(returnMessage==CommonConstant.NULL_VALUE||CommonConstant.EMPTY_VALUE.equals(returnMessage.trim())){
+			baseServiceValidateResult.setMessage(message);
+		}else{
+			baseServiceValidateResult.setMessage(returnMessage);
+		}
+		if(map!=null&&map.size()>0){
+			QueryWapper<T> queryWapper = new QueryWapper<>();
+			for(String key:map.keySet()){
+				queryWapper.eq(key,map.get(key));
+			}
+			List<T> list = daoWapper.selectList(queryWapper);
+			if(list!=null&&list.size()>0){
+				baseServiceValidateResult.setValidatePass(false);
+			}
+		}
+		return baseServiceValidateResult;
+	}
+
+}
