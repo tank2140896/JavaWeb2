ddade52e48a9883ec048ba07d69fb2a6b309fbbf
MrRiver <411833172@qq.com>
2020-12-04 17:46:20 +0800

    Update PatternUtil.java

--- JavaWeb2-common/src/main/java/com/javaweb/util/core/PatternUtil.java
+++ JavaWeb2-common/src/main/java/com/javaweb/util/core/PatternUtil.java
@@ -3,6 +3,8 @@ package com.javaweb.util.core;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
+import com.javaweb.constant.CommonConstant;
+
 public class PatternUtil {
     
     public static final Pattern NUMBER_PATTERN = Pattern.compile("[\\d]+[.]?[\\d]*");//数字的正则
@@ -10,6 +12,9 @@ public class PatternUtil {
     public static final Pattern ZERO_PATTERN = Pattern.compile("[+-]?0.?[0]*");//0的正则
     
     public static boolean isPattern(String str,Pattern pattern) {
+    	if(str==CommonConstant.NULL_VALUE||CommonConstant.EMPTY_VALUE.equals(str)){
+			return false;
+		}
         Predicate<String> predicate = obj -> pattern.matcher(obj).matches();
         return predicate.test(str);
     }
