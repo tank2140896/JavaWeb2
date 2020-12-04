d72b37783ac621b754720d8d8289d743f0d1e943
MrRiver <411833172@qq.com>
2020-12-04 13:42:41 +0800

    Create HandleSelectListCount.java

---
+++ JavaWeb2-database/src/main/java/com/javaweb/mybatis/apiImpl/mysql/HandleSelectListCount.java
@@ -0,0 +1,57 @@
+package com.javaweb.mybatis.apiImpl.mysql;
+
+import java.util.List;
+
+import com.javaweb.enums.DbTypeEnum;
+import com.javaweb.enums.SqlConditionEnum;
+import com.javaweb.exception.ServiceException;
+import com.javaweb.mybatis.apiImpl.SqlBuildInfo;
+import com.javaweb.mybatis.apiImpl.SqlCondition;
+import com.javaweb.mybatis.apiImpl.SqlHandle;
+import com.javaweb.query.QueryWapper;
+
+public class HandleSelectListCount implements SqlHandle {
+
+	public String handle(SqlBuildInfo sqlBuildInfo) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(DbTypeEnum.MYSQL.equals(sqlBuildInfo.getDbTypeEnum())){
+			QueryWapper<?> queryWapper = (QueryWapper<?>)sqlBuildInfo.getParameterValue();
+			List<SqlCondition> sqlConditionList = queryWapper.getSqlConditionList();
+			String tableName = sqlBuildInfo.getTableName();
+			stringBuilder.append("select count(1) from ").append(tableName).append(" where 1=1 ");
+			if(sqlConditionList.size()>0){
+				for(int i=0;i<sqlConditionList.size();i++){
+					SqlCondition sqlCondition = sqlConditionList.get(i);
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.EQ)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" = ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+						break;
+					}
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LIKE)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" like ").append("'%"+sqlCondition.getColumnValue()+"%'");
+						break;
+					}
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LT)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" < ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+						break;
+					}
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.LE)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" <= ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+						break;
+					}
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.GT)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" > ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+						break;
+					}
+					if(sqlCondition.getSqlConditionEnum().compareTo(SqlConditionEnum.GE)==0){
+						stringBuilder.append("and ").append(sqlCondition.getColumnName()).append(" >= ").append(sqlCondition.getColumnValue() instanceof Number ? (sqlCondition.getColumnValue()) : ("'"+sqlCondition.getColumnValue()+"'"));
+						break;
+					}
+				}
+			}
+		}else{
+			throw new ServiceException("目前只支持mysql数据库");
+		}
+		return stringBuilder.toString();
+	}
+
+}
