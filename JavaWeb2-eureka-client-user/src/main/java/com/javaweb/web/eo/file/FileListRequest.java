ddaa3ac0782c1d7a3312d5f4e051d9eb69458d2b
tank2140896 <411833172@qq.com>
2020-12-23 18:49:43 +0800

    no message

---
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/eo/file/FileListRequest.java
@@ -0,0 +1,18 @@
+package com.javaweb.web.eo.file;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileListRequest implements Serializable {
+
+	private static final long serialVersionUID = -2687232983308745956L;
+    
+	private Long currentPage = 1L;//默认当前第1页
+	
+	private Long pageSize = 10L;//默认每页显示10条
+
+}
