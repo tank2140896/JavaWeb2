c26eb9eadcb2f2cd0d3dad029e9f0c1d4f98d103
MrRiver <411833172@qq.com>
2020-12-23 16:52:25 +0800

    Update FileService.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/FileService.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/service/FileService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.javaweb.util.entity.Page;
+import com.javaweb.web.eo.file.FileListRequest;
 import com.javaweb.web.po.User;
 
 public interface FileService {
 	
 	public List<String> uploadFile(MultipartFile multipartFile[],String rootPath,User user) throws Exception;
+	
+	public Page list(FileListRequest fileListRequest);
 	
 }
