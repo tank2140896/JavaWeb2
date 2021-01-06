77edd95e7fdd9de33bfdd0cd507fdac9c0702c3e
MrRiver <411833172@qq.com>
2021-01-06 18:14:27 +0800

    Update Application.java

--- JavaWeb2-integration-demo/src/main/java/com/javaweb/Application.java
+++ JavaWeb2-integration-demo/src/main/java/com/javaweb/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
 @SpringBootApplication(exclude={
 		DataSourceAutoConfiguration.class,//去除数据源默认配置
 		RedisAutoConfiguration.class,//去除redis默认配置
-		RedisRepositoriesAutoConfiguration.class//去除redis默认配置
+		RedisRepositoriesAutoConfiguration.class,//去除redis默认配置
+		MultipartAutoConfiguration.class//去除文件上传默认配置
 })
 public class Application {
 	
