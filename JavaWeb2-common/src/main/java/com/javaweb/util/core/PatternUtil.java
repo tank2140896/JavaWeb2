45ed93fe6d49a1e26ff76dc464cf1b98f7d4a177
tank2140896 <411833172@qq.com>
2021-01-06 19:56:45 +0800

    no message

--- JavaWeb2-common/src/main/java/com/javaweb/util/core/PatternUtil.java
+++ JavaWeb2-common/src/main/java/com/javaweb/util/core/PatternUtil.java
@@ -15,12 +15,21 @@ public class PatternUtil {
     
 	public static final Pattern ZERO_PATTERN = Pattern.compile("[+-]?0.?[0]*");//0的正则
     
+	//是否匹配正则
 	public static boolean isPattern(String str,Pattern pattern) {
 		if(str==CommonConstant.NULL_VALUE||CommonConstant.EMPTY_VALUE.equals(str)){
 			return false;
 		}
 		Predicate<String> predicate = obj -> pattern.matcher(obj).matches();
 		return predicate.test(str);
+	}
+	
+	//正则替换。如将非数字替换为空就是：patternReplace("abc123cba",Pattern.compile("[^0-9]"),"")
+	public static String patternReplace(String str,Pattern pattern,String replacement) {
+		if(str==CommonConstant.NULL_VALUE){
+			return str;
+		}
+		return pattern.matcher(str).replaceAll(replacement).trim();
 	}
 
 }
