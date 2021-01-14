815a87dedb0b20eac21d60c2c35c201d827bf511
MrRiver <411833172@qq.com>
2021-01-14 15:50:29 +0800

    Update WebPermissionInterceptor.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/interceptor/WebPermissionInterceptor.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/interceptor/WebPermissionInterceptor.java
@@ -2,7 +2,6 @@ package com.javaweb.interceptor;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,6 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.javaweb.annotation.url.IgnoreUrl;
-import com.javaweb.base.BaseSystemMemory;
 import com.javaweb.base.BaseTool;
 import com.javaweb.constant.ApiConstant;
 import com.javaweb.constant.CommonConstant;
@@ -43,8 +41,6 @@ public class WebPermissionInterceptor extends HandlerInterceptorAdapter {
         }
         */
 		String servletPath = request.getServletPath();
-		//接口调用次数统计，若不需要使用注释掉即可
-		BaseTool.getRedisTemplate().opsForHash().increment(SystemConstant.REDIS_INTERFACE_COUNT_KEY,specialHandleUrl(servletPath),1);//接口调用次数加1
 		if(ignoreUrl(handler)) {
 	        return true;
 		}
@@ -87,16 +83,4 @@ public class WebPermissionInterceptor extends HandlerInterceptorAdapter {
 	    return false;
 	}
 	
-	//特殊处理URL
-	private static String specialHandleUrl(String url){
-		List<String> baseUrlList = BaseSystemMemory.baseUrlList;
-		for(int i=0;i<baseUrlList.size();i++){
-			if(url.startsWith(baseUrlList.get(i))){
-				url = baseUrlList.get(i);
-				break;
-			}
-		}
-		return url;
-	}
-
 }
