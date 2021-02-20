3a802898d63d8c76939c6f8f805c935538eb623a
MrRiver <411833172@qq.com>
2021-02-20 09:54:31 +0800

    Update User.java

--- JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/po/User.java
+++ JavaWeb2-eureka-client-user/src/main/java/com/javaweb/web/po/User.java
@@ -35,6 +35,9 @@ public class User extends BaseEntity implements Serializable {
 	
 	@Column(name="person_name",columnDesc="用户姓名")
 	private String personName;//用户姓名
+	
+	@Column(name="id_card",columnDesc="身份证号码")
+	private String idCard;//身份证号码
 
 	@Column(name="email",columnDesc="电子邮箱")
 	private String email;//电子邮箱
@@ -51,19 +54,23 @@ public class User extends BaseEntity implements Serializable {
 	@Column(name="fcode",columnDesc="层级关系")
 	private String fcode;//层级关系
 	
-	@Column(name="level",columnDesc="第几级(层级数0为最高,即根节点)")
-	private Integer level;//第几级(层级数0为最高,即根节点)
+	@Column(name="level",columnDesc="第几级（层级数0为最高，即根节点）")
+	private Integer level;//第几级（层级数0为最高，即根节点）
 	
 	@Column(name="remark",columnDesc="备注")
 	private String remark;//备注
 	
-	@Column(name="status",columnDesc="账号状态(0:正常；1：禁用)")
-	private Integer status = 0;//账号状态(0:正常；1：禁用)
+	@Column(name="status",columnDesc="账号状态（0：正常；1：禁用）")
+	private Integer status = 0;//账号状态（0：正常；1：禁用）
+	
+	@Column(name="real_name_status",columnDesc="实名认证状态（0：未实名认证；1：已实名认证）")
+	private Integer realNameStatus = 0;//实名认证状态（0：未实名认证；1：已实名认证）
 	
 	public static final String userIdColumn = "user_id";
 	public static final String userNameColumn = "user_name";
 	public static final String passwordColumn = "password";
 	public static final String personNameColumn = "person_name";
+	public static final String idCardColumn = "id_card";
 	public static final String emailColumn = "email";
 	public static final String phoneColumn = "phone";
 	public static final String portraitColumn = "portrait";
@@ -72,5 +79,6 @@ public class User extends BaseEntity implements Serializable {
 	public static final String levelColumn = "level";
 	public static final String remarkColumn = "remark";
 	public static final String statusColumn = "status";
+	public static final String realNameStatusColumn = "real_name_status";
 
 }
